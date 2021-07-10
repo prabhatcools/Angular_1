0fade64ecef2ca462b93afc810aa3f1673c8d5f2
Prabhat Kumar <prabhatcools@gmail.com>
2021-07-10 17:49:10 +0530

    New project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project1';
+}
